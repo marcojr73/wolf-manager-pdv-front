8b1650469099f59b8126a583f462971f63df31a3
marcojr73 <marcojuniorengineer@gmail.com>
2023-01-20 21:55:11 -0300

    set colors by order

--- src/components/home/elevatedSection/sales.tsx
+++ src/components/home/elevatedSection/sales.tsx
@@ -1,15 +1,14 @@
 import React, { useEffect, useState } from "react"
 import Chart from "react-apexcharts"
+import { BsFillArrowDownCircleFill, BsFillArrowUpCircleFill } from "react-icons/bs"
 import salesApi from "../../../api/salesApi"
 import { Tsales } from "../../../interfaces"
 import ContainerSales from "../../../styles/home/elevatedSection/containerSales"
-import Loader from "../../../utils/Loader"
 import showError from "../../../utils/showError"
 
 function Sales() {
 
     const [sales, setSales] = useState<Tsales>()
-    const [totalBilling, setTotalBilling] = useState(0)
 
 
     useEffect(() => {
@@ -23,6 +22,15 @@ function Sales() {
         })()
     }, [])
 
+    function calculateDifference(initial: number, final: number){
+        const ans = (final - initial) / initial * 100
+        console.log(initial, final)
+        console.log(ans)
+        if (ans > 0) return <div className="percent"><BsFillArrowUpCircleFill className="icon"/><p>{ans.toFixed(2)}%</p></div>
+        else return <div className="percent"><BsFillArrowDownCircleFill className="icon"/><p>{ans.toFixed(2)}%</p></div>
+    }
+
+
     const state = sales !== undefined ? {
         options: {
             chart: {
@@ -30,38 +38,44 @@ function Sales() {
             },
             xaxis: {
                 categories: [
-                    sales.balance[0].date,
-                    sales.balance[1].date,
-                    sales.balance[2].date,
-                    sales.balance[3].date,
-                    sales.balance[4].date,
-                    sales.balance[5].date,
+                    sales.balance[11].date,
+                    sales.balance[10].date,
+                    sales.balance[9].date,
+                    sales.balance[8].date,
+                    sales.balance[7].date,
                     sales.balance[6].date,
                     sales.balance[7].date,
-                    sales.balance[8].date,
-                    sales.balance[9].date,
-                    sales.balance[10].date,
-                    sales.balance[11].date
-                ]
+                    sales.balance[4].date,
+                    sales.balance[3].date,
+                    sales.balance[2].date,
+                    sales.balance[1].date,
+                    sales.balance[0].date
+                ],
+                style: {
+                    colors: ["#F44336", "#E91E63", "#9C27B0"]
+                }
+            },
+            style: {
+                colors: ["#F44336", "#E91E63", "#9C27B0"]
             }
         },
         series: [
             {
                 name: "Vendas",
                 data: [
-                    (sales.balance[0].balance) / 100,
-                    (sales.balance[1].balance) / 100,
-                    (sales.balance[2].balance) / 100,
-                    (sales.balance[3].balance) / 100,
-                    (sales.balance[4].balance) / 100,
-                    (sales.balance[5].balance) / 100,
-                    (sales.balance[6].balance) / 100,
-                    (sales.balance[7].balance) / 100,
-                    (sales.balance[8].balance) / 100,
-                    (sales.balance[9].balance) / 100,
+                    (sales.balance[11].balance) / 100,
                     (sales.balance[10].balance) / 100,
-                    (sales.balance[11].balance) / 100
-                ]
+                    (sales.balance[9].balance) / 100,
+                    (sales.balance[8].balance) / 100,
+                    (sales.balance[7].balance) / 100,
+                    (sales.balance[6].balance) / 100,
+                    (sales.balance[5].balance) / 100,
+                    (sales.balance[4].balance) / 100,
+                    (sales.balance[3].balance) / 100,
+                    (sales.balance[2].balance) / 100,
+                    (sales.balance[1].balance) / 100,
+                    (sales.balance[0].balance) / 100
+                ],
             }
         ],
     } : {
@@ -72,6 +86,7 @@ function Sales() {
     return (
         sales !== undefined ?
             <ContainerSales>
+                <h1>Vendas</h1>
                 <Chart
                     options={state.options}
                     series={state.series}
@@ -81,24 +96,25 @@ function Sales() {
                     className="chart"
                 />
                 <section className="uses">
-                    <div className="box numberSales">
+                    <div className={`box numberSales ${sales.balance[1].numberSales > sales.balance[0].numberSales ? "red" : "green"}`}>
                         <p>N° de Vendas<br />neste mês</p>
-                        <p>{sales.balance[sales.balance.length - 1].numberSales}</p>
+                        <p>{sales.balance[0].numberSales}</p>
+                        <p>{calculateDifference(sales.balance[1].numberSales, sales.balance[0].numberSales)}</p>
                     </div>
-                    <div className="box billing">
+                    <div className={`box billing ${sales.balance[1].balance > sales.balance[0].balance ? "red" : "green" }`}>
                         <p>Faturamento<br />neste mês</p>
-                        <p>{sales.balance[sales.balance.length - 1].balance}</p>
+                        <p>{`${(sales.balance[0].balance)/100} R$`}</p>
+                        <p>{calculateDifference(sales.balance[1].balance, sales.balance[0].balance)}</p>
                     </div>
 
                     <div className="box numberSales">
-                        <p>N° de Vendas<br />neste mês</p>
-                        <p>{sales.balance[sales.balance.length - 1].numberSales}</p>
+                        <p>Faturamento<br />total</p>
+                        <p>{`${(sales.stats.sumBilling)/100} R$`}</p>
                     </div>
                     <div className="box billing">
-                        <p>Faturamento<br />neste mês</p>
-                        <p>{sales.balance[sales.balance.length - 1].balance}</p>
+                        <p>Média de<br />faturamento</p>
+                        <p>{`${((+sales.stats.mediaBilling)/100).toFixed(2)} R$`}</p>
                     </div>
-
                 </section>
             </ContainerSales>
             : null
--- src/styles/home/elevatedSection/containerSales.tsx
+++ src/styles/home/elevatedSection/containerSales.tsx
@@ -5,6 +5,13 @@ const ContainerSales = styled.section`
     height: 50%;
     margin: auto;
 
+    h1{
+        padding-top: 3%;
+        margin-left: -8%;
+        font-size: 2.5rem;
+        color: var(--light-color);
+    }
+
     .chart{
         margin-top: 3%;
         width: 100%;
@@ -15,7 +22,7 @@ const ContainerSales = styled.section`
         grid-template-columns: 1fr 1fr;
         grid-template-rows: 1fr 1fr;
         grid-column-gap: 4%;
-        grid-row-gap: 5%;
+        grid-row-gap: 10%;
         margin: auto;
         margin-top: 3%;
         width: 100%;
@@ -29,10 +36,28 @@ const ContainerSales = styled.section`
         display: flex;
         justify-content: space-between;
         align-items: center;
-        font-size: 2rem;
+        font-size: 1.5rem;
         padding-left: 2%;
         padding-right: 2%;
         color: var(--primary-color);
+    }
+
+    .red{
+        background-color: #f51313;
+    }
+
+    .green{
+        background-color: #30d41a;
+    }
+
+    .percent{
+        display: flex;
+        align-items: center;
+    }
+
+    .icon{
+        font-size: 3rem;
+        margin-right: 5%;
     }
 
 `
